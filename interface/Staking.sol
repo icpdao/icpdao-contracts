cf117b8439af558e00e4f43cae779859a7bd9e25
fushang318 <fushang318@gmail.com>
2021-05-01 22:54:12 +0800

    change

--- interface/Staking.sol
+++ interface/Staking.sol
@@ -31,15 +31,10 @@ contract Staking {
 
     调用者应该是：token 合约，在 token 合约的挖矿接口中调用这个接口
 
-    这个接口做如下三件事情
-    1. 记录质押合约目前持有的 token_address 这个种类 token 的 token 数量
-    2. 因为是 token 合约调用的这个接口，所以可以在这个时候 转账 token 挖矿 的 1% token 给质押合约
-    3. 记录转账后质押合约目前持有的 token_address 这个种类 token 的 token 数量
-      如果新增数量等于 add_amount 说明是一个合法的调用
-      这个涉及到 合约间接调用的设计，很多细节我还不太确定如何写
-    4. 如果 token_address 不在 token_address_list 列表，就增加一下，并创建新的 PoolInfo
-    5. 更新 PoolInfo 中的 accTokenPerShare
-    权限：公开
+    这个接口做如下两件事情
+    1. 如果 token_address 不在 token_address_list 列表，就增加一下，并创建新的 PoolInfo
+    2. 更新 PoolInfo 中的 accTokenPerShare
+    权限：token_address 合约可以调用
   */
   function farm(address token_address, uint256 add_amount) public {
     // TODO
@@ -90,11 +85,16 @@ contract Staking {
     // TODO
   }
 
-  // 计算截止到当前 _user 可以获取 _token_address 对应的 token 种类的分红
-  function pending(address _user, address _token_address)
+  // 计算截止到当前 自己 可以获取 _token_address 对应的 token 种类的分红
+  function pending(address _token_address)
     external
     view
     returns (uint256){
+    // TODO
+  }
+
+  // 提取自己在 _token_address 对应的 token 种类的分红
+  function getPending(address _user, address _token_address) public {
     // TODO
   }
 
