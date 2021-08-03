eae8d3f34009f628c28e39ed6b9d20d81c96ec08
fushang318 <fushang318@gmail.com>
2021-08-03 17:50:47 +0800

    test deploy 3

--- deploy/00_deploy_my_contract.ts
+++ deploy/00_deploy_my_contract.ts
@@ -3,15 +3,26 @@ import {DeployFunction} from 'hardhat-deploy/types';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const {deployments, getNamedAccounts} = hre;
-    const {deploy} = deployments;
+    const {deploy, execute} = deployments;
 
     const {deployer} = await getNamedAccounts();
+    const owner = "0x3946d96a4b46657ca95CBE85d8a60b822186Ad1f";
 
-    await deploy('HelloToken', {
+    // IcpdaoStaking
+    await deploy('IcpdaoStaking', {
         from: deployer,
-        args: ['10000'],
+        args: [owner],
         log: true,
     });
+    const IcpdaoStaking = await deployments.get('IcpdaoStaking');
+
+    // IcpdaoDaoTokenFactory
+    await deploy('IcpdaoDaoTokenFactory', {
+        from: deployer,
+        args: [IcpdaoStaking.address],
+        log: true,
+    });
+
 };
 export default func;
-func.tags = ['HelloToken'];
+func.tags = ['IcpdaoStaking', 'IcpdaoDaoTokenFactory'];
